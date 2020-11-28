6e7ef3f5b032e0f6932925a35c9a906e2118d5b7
Renan Albuquerque <62337593+ren-prog@users.noreply.github.com>
2020-11-28 16:18:13 -0300

    Styles Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,54 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
+
+import logo from "../../assets/logo.svg";
+
+import { Title, Form, Repositories } from "./styles";
 
 // import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title> Explore repositórios no github </Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/62337593?s=460&u=5d39faf678bc8f8ebf40e9d9b53fd95939133538&v=4"
+            alt="Renan"
+          />
+
+          <div>
+            <strong> Rockeseat/unform</strong>
+            <p>Easy peasy highly</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/62337593?s=460&u=5d39faf678bc8f8ebf40e9d9b53fd95939133538&v=4"
+            alt="Renan"
+          />
+
+          <div>
+            <strong> Rockeseat/unform</strong>
+            <p>Easy peasy highly</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+
+import { shade } from "polished";
+
+export const Title = styled.h1`
+  font-size: 50px;
+  color: #3a3a3a;
+
+  max-width: 450px;
+
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 30px 0 0 30px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    border-radius: 0px 30px 30px 0px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, "#04d361")};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 15px;
+    }
+
+    &:hover {
+      transform: translate(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
