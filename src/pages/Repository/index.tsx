ec4a8e26de21ab3decd91c9d163e83c80b5b5a71
Renan Albuquerque <62337593+ren-prog@users.noreply.github.com>
2020-11-30 15:40:39 -0300

    Details styles

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, Link } from "react-router-dom";
+import { FiChevronRight } from "react-icons/fi";
 
-// import { Container } from './styles';
+import { Header, RepositoryInfo, Issues } from "./styles";
+import logo from "../../assets/logo.svg";
 
 interface RepositoryParams {
   repository: string;
@@ -9,7 +11,52 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="" />
+        <Link to="/">
+          <FiChevronRight size={20} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong></strong>
+            <p></p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong></strong>
+            <span></span>
+          </li>
+          <li>
+            <strong></strong>
+            <span></span>
+          </li>
+          <li>
+            <strong></strong>
+            <span></span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdasd">
+          <div>
+            <strong>asdasdasd</strong>
+            <p>asdasdasd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
